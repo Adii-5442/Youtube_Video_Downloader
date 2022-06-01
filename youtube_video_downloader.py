961927a21cee4c1e7fe9c1eb0cbe5b39b1ec03b1
Darkfire5442 <adi.sh5442@gmail.com>
2022-06-01 09:53:20 +0530

    first commit

---
+++ youtube_video_downloader.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import font
+import turtle
+from numpy import pad 
+from pytube import YouTube
+import webbrowser as wb
+
+root = Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title("Youtube Video Downloader")
+root.configure(bg='black')
+
+Label(root,text="YOUTUBE VIDEO DOWNLOADER",bg='black',fg="cyan",font='arial 15 bold').pack()
+
+link = StringVar()
+
+Label(root, text = 'Paste Link Here:',fg='white', font = 'arial 15 bold',bg='black').place(x= 160 , y = 60)
+enteredlink = Entry(root, width = 190,textvariable = link, ).place(x = 32, y = 90 ,height=25)
+
+def Downloader():
+    if link!="":
+        url = YouTube(str(link.get()))
+        video = url.streams.filter(file_extension='mp4').first()
+        video.download()
+        Label(root,text = 'Downloaded Successfully',font='arial 15',fg='black',bg='green').place(x=140,y=210)
+        Label(root,text="By DarkFire",bg="black",fg='cyan',font='arial 15 bold').pack(side="bottom")
+    else:
+        Label(root,text="Please enter a link",bg='black',fg='red').pack(side='bottom')
+
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'red', padx = 2, command = Downloader).place(x=180 ,y = 150)
+
+root.mainloop()
